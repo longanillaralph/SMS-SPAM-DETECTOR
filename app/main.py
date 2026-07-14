50aa49352ac394c6e2987ca82bcb6edb5a336299
EYAN <EYAN@LAPTOP-Q7I12CCV>
2026-07-14 12:45:13 +0800

    Connected it to the database

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.schema import SMSRequest
 from app.predict import predict_sms
+from sqlmodel import Session
+from app.database import engine
+from app.models import Prediction
 
 app = FastAPI()
 
@@ -13,6 +16,14 @@ def home():
 def predict(data: SMSRequest):
 
     prediction, probability = predict_sms(data.message)
+    new_prediction = Prediction(
+    message=data.message,
+    prediction="Spam" if prediction else "Ham",
+    probability=float(probability)
+)
+    with Session(engine) as session:
+        session.add(new_prediction)
+        session.commit()
 
     return {
         "prediction": "Spam" if prediction else "Ham",
